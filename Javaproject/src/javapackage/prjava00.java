2dd48848ce4d69e8ff921c6851c3feda7ebbfabc
Gerard Espinosa <gerard.espinosa.yxart@gmail.com>
2021-10-21 21:02:44 +0200

    Version 1.0

---
+++ Javaproject/src/javapackage/prjava00.java
@@ -0,0 +1,9 @@
+package javapackage;
+
+public class prjava00 {
+	public static void main(String[] args) {
+		System.out.println("Hola món");
+		System.out.println("Versió 0.1 del projecte prjava00");
+		}
+
+}
