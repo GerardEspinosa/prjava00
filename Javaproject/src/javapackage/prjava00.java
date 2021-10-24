26f289786f663e48280a6aa93f610c9db804b561
GerardEspinosa <81532400+GerardEspinosa@users.noreply.github.com>
2021-10-24 22:38:15 +0200

    Version 2.0

--- Javaproject/src/javapackage/prjava00.java
+++ Javaproject/src/javapackage/prjava00.java
@@ -1,9 +1,22 @@
-package javapackage;
-
-public class prjava00 {
+package prjava00;
+import java.net.*;
+public class PrJava00 {
 	public static void main(String[] args) {
-		System.out.println("Hola m�n");
-		System.out.println("Versi� 0.1 del projecte prjava00");
+		System.out.println("Hola món");
+		System.out.println("Versió 0.1 del projecte prjava00");
+		try {
+			InetAddress addr = InetAddress.getLocalHost();
+			String ipAddr = addr.getHostAddress();
+			String hostname = addr.getHostName();
+			System.out.println("hostname="+hostname);
+			System.out.println("Adreça IP: " + ipAddr);
+			System.out.println("Nom de l'usuari: " + System.getProperty("user.name"));
+			System.out.println("Carpeta Personal: " + System.getProperty("user.home"));
+			System.out.println("Sistema operatiu: " + System.getProperty("os.name"));
+			System.out.println("Versió OS: " + System.getProperty("os.version"));
+			}
+		catch (UnknownHostException e) {
+			e.printStackTrace();
+			}
 		}
-
 }
